d287a8754ae51c449b9b3e99ab1573314aaed46a
Brian Swetland <swetland@google.com>
2009-09-15 13:50:31 -0700

    msm: move fixed iomappings and VMALLOC_END to 0xF8000000
    This increases the total virtual footprint available for the 1:1
    physical map and the vmalloc space from 256MB to 896MB, which should
    much improve things for hardware with 256-512MB of ram, discontiguous
    ram banks, etc.
    
    Signed-off-by: Brian Swetland <swetland@google.com>
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/include/mach/msm_iomap.h
+++ arch/arm/mach-msm/include/mach/msm_iomap.h
@@ -43,11 +43,11 @@
 #define IOMEM(x)	((void __force __iomem *)(x))
 #endif
 
-#define MSM_VIC_BASE          IOMEM(0xE0000000)
+#define MSM_VIC_BASE          IOMEM(0xF8000000)
 #define MSM_VIC_PHYS          0xC0000000
 #define MSM_VIC_SIZE          SZ_4K
 
-#define MSM_CSR_BASE          IOMEM(0xE0001000)
+#define MSM_CSR_BASE          IOMEM(0xF8001000)
 #define MSM_CSR_PHYS          0xC0100000
 #define MSM_CSR_SIZE          SZ_4K
 
@@ -55,23 +55,23 @@
 #define MSM_GPT_BASE          MSM_CSR_BASE
 #define MSM_GPT_SIZE          SZ_4K
 
-#define MSM_DMOV_BASE         IOMEM(0xE0002000)
+#define MSM_DMOV_BASE         IOMEM(0xF8002000)
 #define MSM_DMOV_PHYS         0xA9700000
 #define MSM_DMOV_SIZE         SZ_4K
 
-#define MSM_GPIO1_BASE        IOMEM(0xE0003000)
+#define MSM_GPIO1_BASE        IOMEM(0xF8003000)
 #define MSM_GPIO1_PHYS        0xA9200000
 #define MSM_GPIO1_SIZE        SZ_4K
 
-#define MSM_GPIO2_BASE        IOMEM(0xE0004000)
+#define MSM_GPIO2_BASE        IOMEM(0xF8004000)
 #define MSM_GPIO2_PHYS        0xA9300000
 #define MSM_GPIO2_SIZE        SZ_4K
 
-#define MSM_CLK_CTL_BASE      IOMEM(0xE0005000)
+#define MSM_CLK_CTL_BASE      IOMEM(0xF8005000)
 #define MSM_CLK_CTL_PHYS      0xA8600000
 #define MSM_CLK_CTL_SIZE      SZ_4K
 
-#define MSM_SHARED_RAM_BASE   IOMEM(0xE0100000)
+#define MSM_SHARED_RAM_BASE   IOMEM(0xF8100000)
 #define MSM_SHARED_RAM_PHYS   0x01F00000
 #define MSM_SHARED_RAM_SIZE   SZ_1M
 
@@ -85,7 +85,7 @@
 #define MSM_UART3_SIZE        SZ_4K
 
 #ifdef CONFIG_MSM_DEBUG_UART
-#define MSM_DEBUG_UART_BASE   0xE1000000
+#define MSM_DEBUG_UART_BASE   0xF9000000
 #if CONFIG_MSM_DEBUG_UART == 1
 #define MSM_DEBUG_UART_PHYS   MSM_UART1_PHYS
 #elif CONFIG_MSM_DEBUG_UART == 2
@@ -108,15 +108,15 @@
 #define MSM_SDC4_PHYS         0xA0700000
 #define MSM_SDC4_SIZE         SZ_4K
 
-#define MSM_GPIOCFG1_BASE     IOMEM(0xE1004000)
+#define MSM_GPIOCFG1_BASE     IOMEM(0xF9004000)
 #define MSM_GPIOCFG1_PHYS     0xA9000000
 #define MSM_GPIOCFG1_SIZE     SZ_4K
 
-#define MSM_GPIOCFG2_BASE     IOMEM(0xE1005000)
+#define MSM_GPIOCFG2_BASE     IOMEM(0xF9005000)
 #define MSM_GPIOCFG2_PHYS     0xA9100000
 #define MSM_GPIOCFG2_SIZE     SZ_4K
 
-#define MSM_TS_BASE           IOMEM(0xE1006000)
+#define MSM_TS_BASE           IOMEM(0xF9006000)
 #define MSM_TS_PHYS           0xAA300000
 #define MSM_TS_SIZE           SZ_4K
 
@@ -132,30 +132,30 @@
 #define MSM_EMDH_PHYS         0xAA700000
 #define MSM_EMDH_SIZE         SZ_4K
 
-#define MSM_MDP_BASE          IOMEM(0xE0010000)
+#define MSM_MDP_BASE          IOMEM(0xF8010000)
 #define MSM_MDP_PHYS          0xAA200000
 #define MSM_MDP_SIZE          0x000F0000
 
-#define MSM_MDC_BASE	      IOMEM(0xE0200000)
+#define MSM_MDC_BASE	      IOMEM(0xF8200000)
 #define MSM_MDC_PHYS	      0xAA500000
 #define MSM_MDC_SIZE	      SZ_1M
 
-#define MSM_AD5_BASE          IOMEM(0xE0300000)
+#define MSM_AD5_BASE          IOMEM(0xF8300000)
 #define MSM_AD5_PHYS          0xAC000000
 #define MSM_AD5_SIZE          (SZ_1M*13)
 
 #define MSM_UART1DM_PHYS      0xA0200000
 #define MSM_UART2DM_PHYS      0xA0300000
 
-#define MSM_SSBI_BASE         IOMEM(0xE1008000)
+#define MSM_SSBI_BASE         IOMEM(0xF9008000)
 #define MSM_SSBI_PHYS         0xA8100000
 #define MSM_SSBI_SIZE         SZ_4K
 
-#define MSM_TSSC_BASE         IOMEM(0xE1009000)
+#define MSM_TSSC_BASE         IOMEM(0xF9009000)
 #define MSM_TSSC_PHYS         0xAA300000
 #define MSM_TSSC_SIZE         SZ_4K
 
-#define MSM_AXIGS_BASE         IOMEM(0xE100a000)
+#define MSM_AXIGS_BASE         IOMEM(0xF900a000)
 #define MSM_AXIGS_PHYS         0xA8250000
 #define MSM_AXIGS_SIZE         SZ_4K
 
