d405547e8e75f66d68b9600d7d2456acd2445886
HUSSON Pierre-Hugues <phhusson@free.fr>
2009-12-28 00:35:09 +0100

    (vicbush)Fix rhod gpio

--- arch/arm/mach-msm/board-htcrhodium.h
+++ arch/arm/mach-msm/board-htcrhodium.h
@@ -10,9 +10,15 @@
 #include <mach/msm_iomap.h>
 
 
-#define TOPA100_USB_AC_PWR	1
+#define TOPA100_USB_AC_PWR		1
 
-#define TOPA100_BAT_IRQ		28
-#define TOPA100_CHARGE_EN_N	44
+#define TOPA100_BAT_IRQ			28
+#define TOPA100_CHARGE_EN_N		44
+#define RHODIUM_KPD_IRQ			27  //Keyboard IRQ
+#define RHODIUM_KB_SLIDER_IRQ		37  //Keyboard Slider IRQ //Currently Unknown, using stylus detect GPIO right now (37).
+#define RHODIUM_BKL_PWR			86  //Keyboard blacklight  //Currently Unknown if this is right
+
+
+
 
 #endif 
