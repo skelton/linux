78c745b4042eb240457b2bc7209bd4d308ff0377
HUSSON Pierre-Hugues <phhusson@free.fr>
2010-02-25 16:46:12 +0100

    Add kovsky mach id in mmc, pmem and htc_hw driver

--- arch/arm/mach-msm/mmc.c
+++ arch/arm/mach-msm/mmc.c
@@ -479,6 +479,7 @@ int __init init_mmc(void)
 		case MACH_TYPE_HTCRAPHAEL:
 		case MACH_TYPE_HTCDIAMOND_CDMA:
 		case MACH_TYPE_HTCDIAMOND:
+		case MACH_TYPE_HTCKOVSKY:
 		case MACH_TYPE_HTCBLACKSTONE:
 			mmc_pdata = gsm_mmc_pdata;
 			break;
