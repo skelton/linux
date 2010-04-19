c8191cfa036ce222c51884291e22cf0bb79d40df
HUSSON Pierre-Hugues <phhusson@free.fr>
2010-04-19 19:53:04 +0200

    available_memory+=17MB

--- arch/arm/mach-msm/board-htctopaz.c
+++ arch/arm/mach-msm/board-htctopaz.c
@@ -329,7 +329,7 @@ static void __init htctopaz_fixup(struct machine_desc *desc, struct tag *tags,
 	mi->nr_banks++;
 	mi->bank[1].start = PAGE_ALIGN(PHYS_OFFSET + 0x10000000);
 	mi->bank[1].node = PHYS_TO_NID(mi->bank[1].start);
-	mi->bank[1].size = (128 * 1024 * 1024)-51*1024*1024;//See pmem.c for the 51 value
+	mi->bank[1].size = (128 * 1024 * 1024)-34*1024*1024;//See pmem.c for the value
 
 	printk(KERN_INFO "fixup: nr_banks = %d\n", mi->nr_banks);
 	printk(KERN_INFO "fixup: bank0 start=%08lx, node=%08x, size=%08lx\n", mi->bank[0].start, mi->bank[0].node, mi->bank[0].size);
