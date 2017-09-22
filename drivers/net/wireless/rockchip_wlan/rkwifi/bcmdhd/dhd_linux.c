27b90f1f027c4cad7fa582ab43dd83800bf0c9b5
Pierre-Hugues Husson <phh@phh.me>
2017-09-23 00:59:38 +0200

    bcmdhd driver conflicts with esp8089

--- drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/dhd_linux.c
@@ -10230,12 +10230,14 @@ void rockchip_wifi_exit_module_rkwifi(void)
 	dhd_module_exit();
 }
 
+#if 0
 #ifdef CONFIG_WIFI_LOAD_DRIVER_WHEN_KERNEL_BOOTUP
 late_initcall(rockchip_wifi_init_module_rkwifi);
 module_exit(rockchip_wifi_exit_module_rkwifi);
 #else
 module_init(rockchip_wifi_init_module_rkwifi);
 module_exit(rockchip_wifi_exit_module_rkwifi);
+#endif
 #endif
 #if 0
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 0)
